fe4328325e805674c8b8562f0f2d66a9f33c363d
Geary <geary@policystreet.com>
2021-06-05 21:48:03 +0800

    UI UX Projects, links for contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,11 +6,15 @@ export const Contact = () => {
       <h1>Contact Details</h1>
       <div className="contact-details">
         <div className="inline">
-          <i className="fa fa-envelope contact-icon"></i>
+          <a href="mailto:gearyaudie.ga@gmail.com">
+            <i className="fa fa-envelope contact-icon"></i>
+          </a>
           <p>gearyaudie.ga@gmail.com</p>
         </div>
         <div className="inline">
-          <i className="fa fa-phone contact-icon"></i>
+          <a href="https://api.whatsapp.com/send?phone=60143133567&text=Hi%2C%20I%20am%20interested%20with%20your%20work!">
+            <i className="fa fa-phone contact-icon"></i>
+          </a>
           <p>+60 14 313 3567</p>
         </div>
       </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,38 +5,67 @@ export const Projects = () => {
     <div className="projects-sec container" id="projects">
       <h1>Projects</h1>
       <div className="grid-container">
-        <div className="grid-3 proj-card">
-          <a href="https://stark-ocean-04445.herokuapp.com/">
-            <div className="proj-card">
-              <img
-                className="proj-img"
-                src="https://i.ibb.co/cTzbm6L/contact-tracing.png"
-                alt=""
-              />
-              {/* <div className="proj-over">
-              <h3 className="proj-title">Contact Tracing App</h3>
-              <p className="proj-desc">React JS, Firebase</p>
-            </div> */}
+        <div className="grid-3">
+          <div className="proj-container">
+            <a href="https://stark-ocean-04445.herokuapp.com/">
+              <div className="proj-card">
+                <img
+                  className="proj-img"
+                  src="https://i.ibb.co/cTzbm6L/contact-tracing.png"
+                  alt=""
+                />
+              </div>
+            </a>
+            <div className="proj-bg">
+              <h1>Contact Tracing App</h1>
+              <p>React JS, Firebase</p>
             </div>
-          </a>
-          <a href="https://nameless-everglades-73539.herokuapp.com/">
-            <div className="proj-card">
-              <img
-                className="proj-img"
-                src="https://i.ibb.co/yYmpmB4/Screenshot-146.png"
-                alt=""
-              />
+          </div>
+          <div className="proj-container">
+            <a href="https://nameless-everglades-73539.herokuapp.com/">
+              <div className="proj-card">
+                <img
+                  className="proj-img"
+                  src="https://i.ibb.co/yYmpmB4/Screenshot-146.png"
+                  alt=""
+                />
+              </div>
+            </a>
+            <div className="proj-bg">
+              <h1>Weather App</h1>
+              <p>React JS, OpenWeatherMap API</p>
             </div>
-          </a>
-          <a href="https://task-dash.herokuapp.com/">
-            <div className="proj-card">
-              <img
-                className="proj-img"
-                src="https://i.ibb.co/5v22VNC/Screenshot-148.png"
-                alt=""
-              />
+          </div>
+          <div className="proj-container">
+            <a href="https://task-dash.herokuapp.com/">
+              <div className="proj-card">
+                <img
+                  className="proj-img"
+                  src="https://i.ibb.co/5v22VNC/Screenshot-148.png"
+                  alt=""
+                />
+              </div>
+            </a>
+            <div className="proj-bg">
+              <h1>Task Dashboard</h1>
+              <p>CRUD with React JS and Firebases</p>
             </div>
-          </a>
+          </div>
+          <div className="proj-container">
+            <a href="https://task-dash.herokuapp.com/">
+              <div className="proj-card">
+                <img
+                  className="proj-img"
+                  src="https://i.ibb.co/yBBtJPL/Screenshot-158.png"
+                  alt=""
+                />
+              </div>
+            </a>
+            <div className="proj-bg">
+              <h1>Movies Database App</h1>
+              <p>React JS, TMDB API</p>
+            </div>
+          </div>
 
           {/* <img
             className="proj-img"
