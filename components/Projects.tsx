65c7b5bd9305c9825d6820bc928cca21371699d9
Geary <geary@policystreet.com>
2021-07-17 23:15:43 +0800

    Change projects template

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,38 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { projectsList } from "../constants/Projects";
 import classes from "../styles/Projects.module.css";
 
 export const Projects: React.FC = () => {
+  const [expand, setExpand] = useState(false);
+
+  const sliceProjects = projectsList.slice(0, 4);
+
+  const moreProjects = projectsList.slice(Math.max(projectsList.length - 3, 1));
+
+  console.log(moreProjects);
+  console.log(sliceProjects);
+
   return (
     <div className="projects-sec container">
       <div className="projects-pad" id="projects">
         <hr className="line" />
         <div className="grid-container">
-          <div className="grid projects-grid">
+          <div className={classes.projectsGrid}>
             {/* Loop through the projects list */}
-            {projectsList.map((project) => (
-              <a href={project.website}>
-                <div className={classes.imgContainer}>
-                  <img src={project.img} className={classes.img} alt="" />
-                  <div className={classes.imgOverlay}>
-                    <div className={classes.title}>{project.title}</div>
-                    <div className={classes.desc}>{project.desc}</div>
+            {sliceProjects.map((project) => (
+              <>
+                <a href={project.website} key={project.id}>
+                  <div className={classes.imgContainer}>
+                    <img src={project.img} className={classes.img} alt="" />
+                    <div className={classes.imgOverlay}>
+                      <div className={classes.title}>{project.title}</div>
+                      <div className={classes.desc}>{project.desc}</div>
+                    </div>
                   </div>
-                </div>
-              </a>
-              // <div className="proj-container" key={projects.id}>
-              //   <a href={projects.website}>
-              //     <div className="proj-card">
-              //       <img className="proj-img" src={projects.img} alt="" />
-              //     </div>
-              //   </a>
-              //   <div className="proj-bg">
-              //     <h1>{projects.title}</h1>
-              //     <p>{projects.desc}</p>
-              //   </div>
-              // </div>
+                </a>
+              </>
             ))}
+            {expand &&
+              moreProjects.map((project) => (
+                <>
+                  <a href={project.website} key={project.id}>
+                    <div className={classes.imgContainer}>
+                      <img src={project.img} className={classes.img} alt="" />
+                      <div className={classes.imgOverlay}>
+                        <div className={classes.title}>{project.title}</div>
+                        <div className={classes.desc}>{project.desc}</div>
+                      </div>
+                    </div>
+                  </a>
+                </>
+              ))}
           </div>
+          <button className={classes.btn} onClick={() => setExpand(!expand)}>
+            {expand ? "View Less" : "View More"}
+          </button>
           <p className="proj-desc">
             All projects repository are available for view on GitHub
           </p>
